da1c375d79a68848999195d8a84b4afc21c0b416
alan llamas <alanllamasg@gmail.com>
2023-01-19 18:01:58 -0600

    chore: modified collection test for blog post

--- src/api/collection-test/routes/collection-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * collection-test router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::collection-test.collection-test');
--- src/api/collection-test/services/collection-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * collection-test service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::collection-test.collection-test');
