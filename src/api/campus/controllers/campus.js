76b865c883a07ca94906bba7f075efcddf84c92e
sifloz <hecn@live.com.mx>
2023-02-08 14:06:25 -0600

    chore: Implemented components & types for programs offer

---
+++ src/api/campus/controllers/campus.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * campus controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::campus.campus');
---
+++ src/api/campus/routes/campus.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * campus router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::campus.campus');
