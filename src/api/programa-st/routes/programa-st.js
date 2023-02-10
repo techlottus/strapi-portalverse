0cb68f03a0f819d53cf16f9832f8dda081a8cd28
sifloz <hecn@live.com.mx>
2023-02-10 15:14:06 -0600

    chore: Implemented home, blog & blog entry pages

--- src/api/programa-st/routes/programa-st.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * programa-st router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::programa-st.programa-st');
--- src/api/programa-st/services/programa-st.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * programa-st service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::programa-st.programa-st');
