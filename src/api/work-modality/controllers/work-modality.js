edb19c319efcd2d0430c327b6d88cefe4d1ac236
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-02-21 16:11:39 -0600

    feat: create WorkModality collection type

---
+++ src/api/work-modality/controllers/work-modality.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-modality controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::work-modality.work-modality');
