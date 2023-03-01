d6c54eb647af6266b82623708ac721c1b11b64b6
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-03-01 17:40:59 -0600

    feat: create Podcast Episode collection type

---
+++ src/api/podcast-episode/controllers/podcast-episode.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * podcast-episode controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::podcast-episode.podcast-episode');
---
+++ src/api/podcast-episode/routes/podcast-episode.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * podcast-episode router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::podcast-episode.podcast-episode');
