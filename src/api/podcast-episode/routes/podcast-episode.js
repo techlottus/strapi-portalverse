c74590b8012df279e6c746d5ca3a014d2600e559
Holatresveces <eduardo.j.a@ciencias.unam.mx>
2023-03-02 12:43:00 -0600

    Revert "Podcast Episodes Collection Type"

--- src/api/podcast-episode/routes/podcast-episode.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * podcast-episode router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::podcast-episode.podcast-episode');
