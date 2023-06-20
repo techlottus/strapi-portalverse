8e1bfb6cd315170b70425710778f87d42b240271
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-06-20 14:54:54 -0600

    feat: create EducationalOffering single type

---
+++ src/api/educational-offering/services/educational-offering.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * educational-offering service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::educational-offering.educational-offering');
